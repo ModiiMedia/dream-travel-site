ab66f0b79275b94ec4c939461aaafc8e15a9ca51
Joshmossass <joshmsosso@gmail.com>
2020-08-11 11:31:59 -0500

    finalize video grid

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,32 @@
-let bannerContent = document.getElementById("hero-banner-content")
-let bannerBar = document.getElementById("hero-banner-bar")
+const bannerContent = document.getElementById("hero-banner-content");
+const bannerBar = document.getElementById("hero-banner-bar");
 
-resizeBannerBar();
-window.onresize = resizeBannerBar;
+const bannerContentMultipleVideos = document.getElementById(
+	"hero-banner-content-multiple-videos"
+);
+const bannerBarMultipleVideos = document.getElementById(
+	"hero-banner-bar-multiple-videos"
+);
 
-function resizeBannerBar() {
-    let contentHeight = bannerContent.clientHeight
-    if(window.innerWidth <= 1024) {
-        bannerBar.style= `height: ${contentHeight + 80}px;`
-    } else {
-        bannerBar.style = `height: ${contentHeight}px;`
-    }
+resizeBannerBar(bannerBar, bannerContent);
+resizeBannerBar(bannerBarMultipleVideos, bannerContentMultipleVideos);
+window.addEventListener("resize", () => {
+	resizeBannerBar(bannerBar, bannerContent);
+	resizeBannerBar(bannerBarMultipleVideos, bannerContentMultipleVideos);
+});
+
+function resizeBannerBar(bar, content) {
+	if (!bar || !content) {
+		console.error(
+			"Missing parameter(s). Must pass in element for bar and content"
+		);
+		return null;
+	}
+	let contentHeight = content.clientHeight;
+	if (window.innerWidth <= 1024) {
+		bar.style = `height: ${contentHeight + 80}px;`;
+	} else {
+		bar.style = `height: ${contentHeight}px;`;
+	}
+	return null;
 }
